57fdad55747ad9ff002c97fe0c079a5f099d0108
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-04-19 21:33:42 -0600

    Basic file uploading and color finding working. Need to figure out how to remove uploaded file when done.

--- main.py
+++ main.py
@@ -2,21 +2,39 @@
 # Image Color Palette Generator
 # A website that finds the most common colors in an uploaded image.
 
+import os
+
 import cv2 as cv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 from percent_colors import percent_colors
 
+UPLOAD_FOLDER = "./static"
+ALLOWED_EXTENSIONS = {"jpg", "png"}
+
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_PATH"] = 1_000_000
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    image = "gfg-660x249.png"
-    working_image = cv.imread(f"./static/{image}")
+    # os.remove(os.path.join(app.config["UPLOAD_FOLDER"]), '*.*')
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return redirect(url_for("show_image", user_image=filename))
+    return render_template("index.html")
+
+
+@app.route("/showimage/<user_image>")
+def show_image(user_image):
+    working_image = cv.imread(os.path.join(app.config["UPLOAD_FOLDER"], user_image))
     image_colors = percent_colors(working_image)
-    print(image_colors)
-    # return render_template("index.html", colors=image_colors)
+    # os.remove(user_image)
+    return render_template("showimage.html", image=user_image, colors=image_colors)
 
 
 if __name__ == "__main__":
